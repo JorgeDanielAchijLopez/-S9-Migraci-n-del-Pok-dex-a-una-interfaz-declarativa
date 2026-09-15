2a6a546a6deb1569419d7b28bc1633f6bc993109
Daniel Achij <achijdaniel@gmail.com>
2026-09-15 10:44:43 -0600

    feat: crear estructura inicial de Pokedex con React y TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Pokédex React + TypeScript</h1>
+      <p>Migración de la Pokédex a una interfaz declarativa.</p>
+    </main>
+  );
+}
+
+export default App;
